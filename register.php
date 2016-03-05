3f54fdcf477b20d8e0b9d8bdadb5811ead34a9e9
Miko Garcia <crymehonions@gmail.com>
2016-03-06 02:27:31 +0800

    Login and Register
    Implemented Login and Register in the web service

--- register.php
+++ register.php
@@ -1,20 +1,35 @@
 <?php
+	
+	$sel_fname = $_POST[$f_name];
+	$sel_lname = $_POST[$l_name];
+	$sel_pass = $_POST[$password];
+	$sel_email = $_POST[$email];
 
-	require __DIR__."/finditConnect.php";
-	require __DIR__."/account.php";
-	require __DIR__."/getAccount.php";
-
-	$sql = "INSERT INTO $account_table ($f_name, $l_name, $password, $email, $acc_type) 
-		VALUES ($_POST[$f_name], $_POST[$l_name], $_POST[$password], $_POST[$email], $_POST[$acc_type])";
-
-	if($conn->query($sql) == TRUE) {
-		$last_id = $conn->insert_id;
-
-		echo getAccount($last_id);
+	if($sel_fname == '' || $sel_lname == '' || $sel_pass == '' || $sel_email == '') {
+		echo "Fill all fields";
 	} else {
-		echo "Error: ".$sql."<br>".$conn->error;
-	}
+		require __DIR__."/finditConnect.php";
+		require __DIR__."/account.php";
+		require __DIR__."/getAccount.php";
 
-	$conn()->close();
+		$sql = "SELECT $a_acc_id FROM $account_table WHERE $email = '$sel_email'";
+		$result = $conn->query($sql);
+		if($result->num_rows > 0) {
+			echo "Email already being used!";
+		} else {
+			$sql = "INSERT INTO $account_table ($f_name, $l_name, $password, $email) 
+			VALUES ($sel_fname, $sel_lname, $sel_pass, $sel_email)";
+
+			if($conn->query($sql) == TRUE) {
+				$last_id = $conn->insert_id;
+
+				echo getAccount($last_id);
+			} else {
+				echo $conn->error;
+			}
+		}
+
+		$conn()->close();
+	}
 
 ?>
