d447fca240bb2d040f7a7cbb3c71f43563c05dc7
Miko Garcia <crymehonions@gmail.com>
2016-03-17 01:00:18 +0800

    Fixed insertReports

--- insertReport.php
+++ insertReport.php
@@ -10,28 +10,35 @@
 	$sel_r_type = $_POST[$report_type];
 	$sel_i_type = $_POST[$item_type];
 	$sel_acc = $_POST[$r_acc_id];
-	$features = array($_POST[$feature_table]);
 
 	require __DIR__."/finditConnect.php";
-	require __DIR__."/getReport.php"
+	require __DIR__."/getReport.php";
 
-	$sql = "INSERT INTO $report_table ($r_acc_id, $item_name, $item_type, $report_place, $report_date, $report_type, $log_date) 
-	VALUES ($sel_acc, $sel_item, $sel_i_type, $sel_place, $sel_date, $sel_r_type, CURTIME())";
+	$sql = "INSERT INTO $report_table ($r_acc_id, $item_name, $item_type, $report_place, $report_date, $report_type, $log_date) VALUES ($sel_acc, '$sel_item', $sel_i_type, '$sel_place', '$sel_date', $sel_r_type, CURTIME())";
 
 	if($conn->query($sql) == TRUE) {
 		$lastid = $conn->insert_id;
 
-		foreach ($features as $feat) {
-			$sql = "INSERT INTO $feature_table ($f_report_id, $feature) VALUES ($lastid, $feat)";
+		if(!empty($_POST[$feature_table])) {
 
-			if($conn->query($sql) != TRUE) {
-				echo "ERROR: Feature not added!";
+			foreach ($_POST[$feature_table] as $feat) {
+				$sql = "INSERT INTO $feature_table ($f_report_id, $feature) VALUES ($lastid, '$feat')";
+
+				if($conn->query($sql) != TRUE) {
+					echo "ERROR: Feature not added!\n";
+					echo $conn->error;
+				}
 			}
+
+			echo getReport($lastid);
+		} else {
+			echo getReport($lastid);
 		}
 
-		echo getReport($lastid);
 	} else {
-		echo "ERROR: Report not added!";
+		echo "ERROR: Report not added!\n";
+		echo $sql."\n";
+		echo $conn->error;
 	}
 
 	$conn->close();
