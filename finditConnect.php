8588b4c99682cb9b821ea8fbb2fa51a39ff56d1a
Miko Garcia <crymehonions@gmail.com>
2016-04-14 13:39:15 +0800

    Reverts

--- finditConnect.php
+++ finditConnect.php
@@ -3,7 +3,7 @@
 	#CHANGE THESE TO WHAT YOU SET ON YOUR MACHINE
 	$dbhost = "localhost:3306";
 	$dbuser = "root";
-	$dbpass = "1234";
+	$dbpass = "p@ssword";
 	
 	$dbname = "findit";
 
--- possibleMatches.php
+++ possibleMatches.php
@@ -22,50 +22,47 @@
 	}
 
 	$sql = "SELECT * FROM $report_table
-			WHERE $report_type <> $sel_r_type
-			AND $item_type = $sel_type
+			WHERE $report_type = 2
 			AND (
 				$item_name LIKE '$sel_name'
 				OR $report_date = $sel_date
 				OR $report_place LIKE '$sel_place'
+				OR $item_type = $sel_type
 			)";
 
 	$result = $conn->query($sql);
 	$xmlData = "<?xml version = '1.0' encoding = 'UTF-8'?>
-	<Content>";
+	<Matches>";
 	if($result->num_rows > 0) {
 		foreach ($result as $row) {
+			$sql = "SELECT * FROM $feature_table WHERE $f_report_id = $row[$r_report_id]";
+			
+			$feat_result = $conn->query($sql);
+
 			$xmlData = "$xmlData
-			<$report_table>
+			<Report>
 			<$r_report_id>$row[$r_report_id]</$r_report_id>
-			<$report_type>$row[$report_type]</$report_type>
 			<$item_name>$row[$item_name]</$item_name>
 			<$item_type>$row[$item_type]</$item_type>
 			<$log_date>$row[$log_date]</$log_date>
 			<$report_place>$row[$report_place]</$report_place>
-			<$report_date>$row[$report_date]</$report_date>
-			<$claimed>$row[$claimed]</$claimed>";
+			<$report_date>$row[$report_date]</$report_date>";
 
-			$sql = "SELECT * FROM $feature_table WHERE $f_report_id = $row[$r_report_id]";  
-		
-			$feat_result = $conn->query($sql);
-			if($feat_result->num_rows > 0) {
-				foreach ($feat_result as $f_row) {
-					$xmlData = "$xmlData
-					<$feature_table>
-					<$f_feat_id>$f_row[$f_feat_id]</$f_feat_id>
-					<$feature>$f_row[$feature]</$feature>
-					</$feature_table>";
+				if($feat_result->num_rows > 0) {
+					foreach ($feat_result as $f_row) {
+						$xmlData = "$xmlData
+						<$feature_table>
+						<$f_feat_id>$f_row[$f_feat_id]</$f_feat_id>
+						<$feature>$f_row[$feature]</$feature>
+						</$feature_table>";
+					}
 				}
-			} else {
-				echo $conn->error;
-			}
-			
-			$xmlData = $xmlData."</$report_table>";
+				
+			$xmlData = $xmlData."</Report>";
 		}
 	}
 
-	$xmlData = $xmlData."</Content>";
+	$xmlData = $xmlData."</Matches>";
 	$xml = new SimpleXMLElement($xmlData) or die("Cannot Create Object");
 	$json = json_encode($xml);
 
